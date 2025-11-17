cc617c61088d397499f7d83aa8dce16796f0f778
Godstime <98828211+iceberg61@users.noreply.github.com>
2025-11-17 01:02:50 +0100

    Update route.js

--- app/api/admin/sales/route.js
+++ app/api/admin/sales/route.js
@@ -1,20 +1,20 @@
-import dbConnect from '@/lib/dbConnect';
-import Purchase from '@/models/Purchase';
-import { NextResponse } from 'next/server';
-import { verifyAdmin } from '@/lib/verifyAdmin';
+// import dbConnect from '@/lib/dbConnect';
+// import Purchase from '@/models/Purchase';
+// import { NextResponse } from 'next/server';
+// import { verifyAdmin } from '@/lib/verifyAdmin';
 
-export async function GET() {
-  await dbConnect();
+// export async function GET() {
+//   await dbConnect();
 
-  const admin = verifyAdmin();
-  if (!admin) {
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 403 });
-  }
+//   const admin = verifyAdmin();
+//   if (!admin) {
+//     return NextResponse.json({ error: 'Unauthorized' }, { status: 403 });
+//   }
 
-  const sales = await Purchase.find()
-    .populate('user', 'username email')
-    .populate('log', 'title price')
-    .sort({ createdAt: -1 });
+//   const sales = await Purchase.find()
+//     .populate('user', 'username email')
+//     .populate('log', 'title price')
+//     .sort({ createdAt: -1 });
 
-  return NextResponse.json({ sales });
-}
+//   return NextResponse.json({ sales });
+// }
